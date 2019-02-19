81864ad00ab18792d20530a50d02f1c51f3a57f4
Alex Fedotov <alxfed@gmail.com>
2019-02-19 17:44:15 -0600

    first regression

--- regression/generator_make_regression.py
+++ regression/generator_make_regression.py
@@ -10,16 +10,25 @@ import matplotlib.pyplot as plt
 
 import sklearn as sk
 # print('Scikit Learn:{}'.format(sk.__version__))
-from sklearn import model_selection as skms
 from sklearn import datasets as skds
-from sklearn import preprocessing as skpp
 
+# the function make_regression returns a tuple
 X, y = skds.make_regression(
-    n_samples=200, n_features=1, n_informative=1, n_targets=1, noise=20.0)
+    n_samples=200,
+    n_features=1,
+    n_informative=1,
+    n_targets=1,
+    noise=20.0)
+
+print(X, "\n")
+print(y, "\n")
+
+# reshape?
 if (y.ndim == 1):
     y = y.reshape(-1, 1)
 
+# plot the points
 plt.figure(figsize=(14,8))
 plt.plot(X,y,'b.')
-plt.title('Original Dataset')
+plt.title('The Synthetic Dataset')
 plt.show()
