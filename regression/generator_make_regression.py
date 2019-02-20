8db52f68ae15a565fb3e95675b65dd7e07616eae
alxfed <alxfed@gmail.com>
2019-02-19 19:40:15 -0600

    added some

--- regression/generator_make_regression.py
+++ regression/generator_make_regression.py
@@ -8,9 +8,9 @@ https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_regressi
 
 """
 
-import numpy as np
+#import numpy as np
 # print('NumPy:{}'.format(np.__version__))
-np.random.seed(123)
+# np.random.seed(123)
 
 import matplotlib.pyplot as plt
 
@@ -23,7 +23,7 @@ from sklearn import datasets as skds
 # the _generated_ y matrix is called 'targets'
 
 X, y = skds.make_regression(
-    n_samples=10,       # integer, default = 100
+    n_samples=100,       # integer, default = 100
     n_features=1,       # integer, default = 100
     n_informative=1,    # integer, default = 10
                         # the number of features used to build the linear
@@ -44,15 +44,19 @@ X, y = skds.make_regression(
                         # effective_rank is not None.
     noise=0.0,          # the standard deviation of Gaussian noise, added
                         # to the output - y.
-    random_state=1)     # the seed-like integer for reproducibility
+#   coef=True,          # boolean, optional (default=False)
+                        #   If True, the coefficients of the underlying
+                        #   linear model are returned.
+    random_state=789)   # the seed-like integer for reproducibility
 
 print(X, "\n")
 print(y, "\n")
 
 # reshape?
 if (y.ndim == 1):
-    y = y.reshape(-1, 1) # ?
+    y = y.reshape(-1, 1) # Transposes y which originally is a line (not column).
 
+print(y, "\n")
 # plot the points
 plt.figure(figsize=(14,8))
 plt.plot(X,y,'b.')
@@ -61,8 +65,14 @@ plt.show()
 
 """
 Now I understand.
-the "features" are generated to be Gaussian and"centered" around zero and
-having a standard deviation of 1, which means that they can be positive, 
+the "features" are generated to be Gaussian and "centered" (around zero)
+and having a standard deviation of 1, which means that they can be positive, 
 negative and are potentially unlimited. This fake 'input' is put through
 a linear model generated in a way that a standart deviation of 1 would 
+transform into a 'target' of approximately 100. The 'bias' is the "b" term
+of the linear model so it shifts the picture by the same number of units
+
+What I don't understand is: why not define the coefficients of the linear
+model? Why not let scale the output? 
+Technical: I tried to output the coefficients of the linear 
 """
